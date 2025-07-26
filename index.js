e6edd06261a4580414a681fdcf9a738e1ae6b320
mustafa siam <mustafasiam357@gmail.com>
2025-07-27 00:07:20 +0600

    find spcific food

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -39,6 +39,12 @@ async function run() {
     })
     app.get('/allcuisin',async(req,res)=>{
       const result=await cuisinecollection.find().toArray();
+      res.send(result)
+    })
+    app.get('/allcuisin/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await cuisinecollection.findOne(query);
       res.send(result)
     })
     // Send a ping to confirm a successful connection
